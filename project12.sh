a6835c72bf4a39d41a0713cabc5ca8d5aa5854aa
audrey <Audreytchoumessi@gmail.com>
2020-09-02 17:35:41 -0400

    change

---
+++ project12.sh
@@ -0,0 +1,12 @@
+1-
+A script is a list of command that are executed by a certains program or scripting engine use to autimate processes on a local computer or to generate pages on the web
+
+2-To run a script that does not have execute permission we will do #bashfile.sh or sudo bash file.sh
+
+3-For loop is a program luanguage conditional iterative statement which is use to check for certain condition and then repeatdly execute a block of code as lonf as those condition are met
+
+4-To close and if statement we use Fi
+
+5-Awk commend is used to extract or filter data in a command output and the Set command is use to edit a file
+
+6-
